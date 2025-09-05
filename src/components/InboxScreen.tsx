4d7f269be7740d95b1ae50aa89b99d5dcaeb8cd8
jimmylgz <1091990@qq.com>
2025-09-05 11:04:44 +0800

    Update title in InboxScreen component to reflect latest tasks

--- src/components/InboxScreen.tsx
+++ src/components/InboxScreen.tsx
@@ -27,7 +27,7 @@ export default function InboxScreen() {
   return (
     <div className="page lists-show">
       <nav>
-        <h1 className="title-page">Taskbox</h1>
+        <h1 className="title-page">Taskbox Latest</h1>
       </nav>
       <TaskList />
     </div>
